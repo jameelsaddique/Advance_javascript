17b53b44988c0f5aa184d8bb0543776536015a2f
Habib <habibsaddique4410@gmail.com>
2026-04-23 07:16:19 +0500

    Switch statement is added

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-const playerGuess = 10;
-const correctNumber = 10;
-let message = '';
-const finalMessage = (playerGuess > correctNumber) ? 'Too high!' : (playerGuess < correctNumber) ? 'Too low!' : 'Congratulations! You guessed the correct number.';
-console.log(finalMessage)
+// I practice ternary operator 
+
+// const playerGuess = 10;
+// const correctNumber = 10;
+// let message = '';
+// const finalMessage = (playerGuess > correctNumber) ? 'Too high!' : (playerGuess < correctNumber) ? 'Too low!' : 'Congratulations! You guessed the correct number.';
+// console.log(finalMessage)
+
+//I practice Switch statement with an example
+
+// function priceList(item){
+//     let price = 0;
+//     switch(item){
+//         case 'coffee':
+//             price = 3;
+//             break;
+//         case 'Sandwich':
+//             price = 5;
+//             break;
+//         case 'Salad':
+//             price = 4;
+//             break;
+//         default:
+//             price = "Sorry We don't sales this item"
+        
+//     }
+    
+//     const finalPrice = `The price of ${item} is $${price}.`
+//     return finalPrice;
+// }
+// console.log(priceList('lemon'))
+
+// Now I study about Object destructuring
+
